6e585f29171bb19004b88ca3f914443202889306
Jay Khan <jkhan992@gmail.com>
2018-08-08 01:16:37 +0100

    Integrate Create User API

--- jasmine/jasmine-standalone-3.1.0/src/peeps-controller.js
+++ jasmine/jasmine-standalone-3.1.0/src/peeps-controller.js
@@ -38,6 +38,38 @@
     });
   }
 
-  exports.PeepsController = PeepsController;
+  PeepsController.prototype.createUserForm = function(){
+    document.getElementById('create-user').addEventListener('click', function() {
+      var output = ""
 
+        output += `<div><form id="CreateUser"><input type="text" id="handle" name="handle"><input type="text" id="password" name="password"><input class="button" name="submit" type="submit" value="submit22" onclick="PeepsController.prototype.getFormData()" /></form></div>`
+
+        appDivTitle.innerHTML = `<h2>Create a user</h2>`;
+        appDivBody.innerHTML = $(document.body).append(output);
+        return appDivBody.innerHTML;
+    });
+  }
+
+  PeepsController.prototype.getFormData = function(){
+    var handle = document.getElementById('handle').value;
+    var password = document.getElementById('password').value;
+    
+    PeepsController.prototype.createUser(handle, password);
+  }
+
+  PeepsController.prototype.createUser = function(handle, password){
+    var handle;
+    var password;
+
+    $.ajax({
+      url: 'https://chitter-backend-api.herokuapp.com/users',
+      header: {
+        'Content-Type': 'application/json'
+      },
+      method: 'POST',
+      data: {user: {handle: handle, password: password} },
+    });
+  }
+
+  exports.PeepsController = PeepsController;
 })(this);
